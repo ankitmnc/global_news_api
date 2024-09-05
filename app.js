229eea5883285f99f589057e1b2236f3aca13bc6
ankit4548 <20je0147@am.iitism.ac.in>
2024-09-06 03:40:11 +0530

    modified request type

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ app.listen(port, "0.0.0.0", () => {
     console.log(`Server started listening at ${port}.`);
 });
 
-app.post('/get_news',(req,res)=>{
+app.get('/get_news',(req,res)=>{
     var category = req.query.category; 
+    console.log(category);
     axios
     .get(`https://newsapi.org/v2/everything?q=${category}&apiKey=e719b9cf112540d59dc4ca8e87e383ce`)
     .then((resp)=>{
-        console.log(resp.data);
         res.send(resp.data);
     })
     .catch((e)=>console.log(e));
